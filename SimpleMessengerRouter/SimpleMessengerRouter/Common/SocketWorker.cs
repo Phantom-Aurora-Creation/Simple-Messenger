d1cf93d8ec8fe1f99bf48107087dd08c821f72c4
Romonov <Romonov2003@outlook.com>
2018-10-27 13:10:24 +0800

    请求路由器

---
+++ SimpleMessengerRouter/SimpleMessengerRouter/Common/SocketWorker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMessengerRouter.Common
+{
+    class SocketWorker
+    {
+        private readonly Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        public Socket GetSocket()
+        {
+            return socket;
+        }
+
+        public void Init(int Port)
+        {
+            socket.Bind(new IPEndPoint(IPAddress.Any, Port));
+        }
+
+        public void Listen(int Num)
+        {
+            socket.Listen(Num);
+        }
+
+    }
+}
---
+++ SimpleMessengerRouter/SimpleMessengerRouter/Program.cs
@@ -0,0 +1,23 @@
+﻿using SimpleMessengerRouter.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMessengerRouter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocketWorker Socket = new SocketWorker();
+            Socket.Init(37201);
+            Socket.Listen(233);
+
+
+
+            Logger.Info("Welcome!");
+        }
+    }
+}
